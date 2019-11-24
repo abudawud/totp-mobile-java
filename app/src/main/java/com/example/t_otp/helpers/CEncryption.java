59114ecc30c464580d0e786226bfdea0b164e018
Abu Dawud <warishafidz@gmail.com>
2019-11-24 23:52:35 +0700

    fix encryption and integrated

--- app/src/main/java/com/example/t_otp/helpers/CEncryption.java
+++ app/src/main/java/com/example/t_otp/helpers/CEncryption.java
@@ -6,8 +6,15 @@ import android.util.Log;
 import com.example.t_otp.BuildConfig;
 import com.example.t_otp.MainActivity;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -40,12 +47,66 @@ public class CEncryption {
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
             byte[] encrypted = cipher.doFinal(plainText.getBytes());
+
             return Base64.encodeToString(encrypted, Base64.DEFAULT);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return null;
 
+    }
+
+    public static String decrypt(String cipher, boolean isPrivate){
+        final Integer keyLife = BuildConfig.OTP_KEY_LIFE;
+        Integer timeForce = BuildConfig.OTP_KEY_FORCE;
+        final Integer factor = timeForce / 2;
+        final Integer ctime = ((int)(System.currentTimeMillis() / 1000)) + factor;
+        Integer counter = (ctime / keyLife) + factor;
+
+        String cKey;
+        String clearText;
+
+        if(isPrivate){
+            cKey = MainActivity.OTP_PRIVATE_KEY;
+        }else{
+            cKey = BuildConfig.OTP_MAIN_KEY;
+        }
+
+        while (timeForce-- > 0){
+            clearText = tryDecrypt(cipher, cKey, counter);
+            if(clearText != null){
+                Log.d(TAG, "DECRYPT SUCCESS: " + clearText);
+                return clearText;
+            }
+
+            counter--;
+        }
+
+        Log.d(TAG, "decrypt: FAILED DECRYPT");
+
+        return null;
+    }
+
+    private static String tryDecrypt(String cipher, String tkey, final Integer counter){
+        try {
+            Mac mac = Mac.getInstance("HmacMD5");
+            mac.init(new SecretKeySpec(hexStringToByteArray(tkey), "HmacMD5"));
+
+            mac.update(counter.toString().getBytes());
+            byte [] key = mac.doFinal();
+            IvParameterSpec iv = new IvParameterSpec(hexStringToByteArray(BuildConfig.OTP_INIT_VECTOR));
+            SecretKeySpec skeySpec = new SecretKeySpec(key, BuildConfig.OTP_ALGORITHM);
+
+            Cipher mcipher = Cipher.getInstance(BuildConfig.OTP_CHIPER);
+            mcipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            return new String(mcipher.doFinal(Base64.decode(cipher, Base64.DEFAULT)));
+        } catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException | IllegalBlockSizeException ex){
+            return null;
+        }
+
+        return null;
     }
 
     private static byte[] hexStringToByteArray(String s) {
